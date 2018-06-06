1efb22940985398159643416056d15682ff41ad6
Gustavo Varo <gustavo.varo@microsoft.com>
2018-06-06 14:10:51 -0400

    fixing a small bug when commit author has accented chars in the name.

--- server-infra/buildtask_scripts/removeolddockerimages.ps1
+++ server-infra/buildtask_scripts/removeolddockerimages.ps1
@@ -1,6 +1,6 @@
 # This script is responsible to remove all images older than 24 hours.
 
-$knownImages = "ubuntu", "everest_base_image", "fstar_base_image"
+$knownImages = "ubuntu", "everest_base_image", "fstar_base_image", "mitls_base_image"
 
 $images = docker images --format '{{json .}}' | ConvertFrom-Json
 $images | ForEach-Object {
