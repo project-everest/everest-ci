139bb721eaa70593586d8287b89718e6990ef64a
Barry Bond <barrybo@microsoft.com>
2018-06-15 14:51:17 -0700

    Tweaks to work on Windows

--- server-infra/buildtask_scripts/extractfstarquerystatslogsfromcontainer.ps1
+++ server-infra/buildtask_scripts/extractfstarquerystatslogsfromcontainer.ps1
@@ -12,8 +12,13 @@ param
 docker run -t -d --name $BuildRequestedAuthor $ContainerImageName
 
 # Copy buildlog file.
+if ($Env:OS -eq "Windows_NT") {
+docker exec $BuildRequestedAuthor cmd.exe /c type log_no_replay.html > log_no_replay.html
+docker exec $BuildRequestedAuthor cmd.exe /c type log_worst.html > log_worst.html
+} else {
 docker exec $BuildRequestedAuthor cat log_no_replay.html > log_no_replay.html
 docker exec $BuildRequestedAuthor cat log_worst.html > log_worst.html
+}
 
 # Stop Container
 docker stop $BuildRequestedAuthor
