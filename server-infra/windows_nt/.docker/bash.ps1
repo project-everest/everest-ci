a6c893fa0289735fd3987c363e03b46d2742b19a
Gustavo Varo <gustavo.varo@microsoft.com>
2018-08-24 14:24:45 -0400

    Fix bash.ps1 to report errors

--- server-infra/windows_nt/.docker/bash.ps1
+++ server-infra/windows_nt/.docker/bash.ps1
@@ -1,3 +1,11 @@
+$Error.Clear()
+$LastExitCode = 0
+
+Write-Host "Arguments: " $args
 $cygpath = c:\cygwin64\bin\cygpath.exe -u ${pwd}
 c:\cygwin64\bin\bash.exe --login -c "cd $cygpath && $args"
 
+if ($Error.Count -gt 0 -or $LastExitCode -ne 0) {
+    $Error
+    exit 1
+}
