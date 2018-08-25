f8332cb8ce5aee430da854652a1dbe1ae827f6ac
Gustavo Varo <gustavo.varo@microsoft.com>
2018-08-25 00:19:21 -0400

    Fix windows docker container

--- server-infra/windows_nt/.docker/bash.ps1
+++ server-infra/windows_nt/.docker/bash.ps1
@@ -1,11 +1,14 @@
 $Error.Clear()
 $LastExitCode = 0
 
-Write-Host "Arguments: " $args
 $cygpath = c:\cygwin64\bin\cygpath.exe -u ${pwd}
-c:\cygwin64\bin\bash.exe --login -c "cd $cygpath && $args"
+$cmd = "cd $cygpath && $args"
+Write-Host "Command to execute: " $cmd
+
+c:\cygwin64\bin\bash.exe --login -c "$cmd"
 
 if ($Error.Count -gt 0 -or $LastExitCode -ne 0) {
+    Write-Host "*** Error:"
     $Error
     exit 1
 }
