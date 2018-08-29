10a2691d221d7d1372761cedb86039bcefb579fb
Gustavo Varo <gustavo.varo@microsoft.com>
2018-08-29 01:21:06 -0400

    Some fixes on the windows container

--- server-infra/windows_nt/.docker/bash.ps1
+++ server-infra/windows_nt/.docker/bash.ps1
@@ -1,14 +1,32 @@
-$Error.Clear()
-$LastExitCode = 0
+function global:Invoke-BashCmd
+{
+    $Error.Clear()
+    $LastExitCode = 0
 
-$cygpath = c:\cygwin64\bin\cygpath.exe -u ${pwd}
-$cmd = "cd $cygpath && $args"
-Write-Host "Command to execute: " $cmd
+    Write-Host "Args:" $args
 
-c:\cygwin64\bin\bash.exe --login -c "$cmd"
+    # Escape quotes
+    $args = $args -replace '"','""'
+    $args = $args -replace "'",'""'
 
-if ($Error.Count -gt 0 -or $LastExitCode -ne 0) {
-    Write-Host "*** Error:"
-    $Error
-    exit 1
+    # Exec command
+    $outputFile = "output.txt"
+    $cygpath = c:\cygwin64\bin\cygpath.exe -u ${pwd}
+    c:\cygwin64\bin\bash.exe --login -c "cd $cygpath && $args" | Out-File $outputFile
+
+    If ((Test-Path $outputFile) -eq $true) {
+        Get-Content $outputFile -Raw | Write-Host
+        Remove-Item $outputFile -Force
+    }
+
+    if ($Error.Count -gt 0 -or $LastExitCode -ne 0) {
+        Write-Host "*** Error:"
+        $Error
+        exit 1
+    }
+}
+
+function global:New-BashCmdProfile
+{
+    cat bash.ps1 >> $profile.AllUsersCurrentHost
 }
--- server-infra/windows_nt/.docker/installopam.sh
+++
@@ -1,6 +0,0 @@
-cd /cygdrive/c/ewerest/opam
-tar -xf opam.tar.xz
-bash opam64/install.sh
-opam init mingw 'https://github.com/fdopen/opam-repository-mingw.git' --comp $OCAML_VER+mingw64c --switch $OCAML_VER+mingw64c -y > opaminstall.log 2>&1
-echo ". '/cygdrive/c/users/everest/.opam/opam-init/init.sh' > /dev/null 2> /dev/null || true" >>/cygdrive/c/users/everest/.bash_profile
-
