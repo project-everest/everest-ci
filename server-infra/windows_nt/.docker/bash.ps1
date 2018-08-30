b9cac7c890d986f6dc372b719083523f0d63f422
Gustavo Varo <gustavo.varo@microsoft.com>
2018-08-30 11:03:35 -0400

    Fixing cygwin output

--- server-infra/windows_nt/.docker/bash.ps1
+++ server-infra/windows_nt/.docker/bash.ps1
@@ -7,17 +7,11 @@ function global:Invoke-BashCmd
 
     # Escape quotes
     $args = $args -replace '"','""'
-    $args = $args -replace "'",'""'
+    $args = $args -replace "'","''"
 
     # Exec command
-    $outputFile = "output.txt"
     $cygpath = c:\cygwin64\bin\cygpath.exe -u ${pwd}
-    c:\cygwin64\bin\bash.exe --login -c "cd $cygpath && $args" | Out-File $outputFile
-
-    If ((Test-Path $outputFile) -eq $true) {
-        Get-Content $outputFile -Raw | Write-Output
-        Remove-Item $outputFile -Force
-    }
+    c:\cygwin64\bin\bash.exe --login -c "cd $cygpath && $args" | Write-Output
 
     if ($Error.Count -gt 0 -or $LastExitCode -ne 0) {
         Write-Host "*** Error:"
