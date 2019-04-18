2e60ba8390ecb20057e241c52cc326547b032a19
Gustavo Varo <gustavo.varo@microsoft.com>
2019-04-18 11:57:31 -0400

    Add zoom capability to perf chart

--- perf-track/chart/chart.js
+++ perf-track/chart/chart.js
@@ -1,3 +1,15 @@
+'use strict';
+
+window.chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
+
 /// Gets the url variables
 function getUrlVars() {
     var vars = [], hash;
@@ -21,9 +33,19 @@ function downloadPerfData(buildId) {
         if (data !== undefined && data !== null) {
             var obj = jQuery.parseJSON( data );
             var array = [];
+            array.push(new Array());
+            array.push(new Array());
+            array.push(new Array());
 
             $(obj).each(function(i, val) {
-                array.push({
+                var index = 0;
+                if (val.PreviousMetricValue == val.MetricValue) {
+                    index = 1;
+                } else if (val.PreviousMetricValue > val.MetricValue) {
+                    index = 2;
+                }
+
+                array[index].push({
                     index : i,
                     QueryName: val.QueryName,
                     MetricName: val.MetricName,
@@ -40,16 +62,73 @@ function downloadPerfData(buildId) {
                 type: 'scatter',
                 data: {
                     datasets: [{
-                        label: 'rlimit-count',
-                        data: array
+                        label: 'worse',
+                        borderColor: window.chartColors.red,
+                        data: array[0]
+                    },
+                    {
+                        label: 'equal',
+                        borderColor: window.chartColors.blue,
+                        data: array[1]
+                    },
+                    {
+                        label: 'better',
+                        borderColor: window.chartColors.green,
+                        data: array[2]
                     }]
                 },
                 options: {
+                    title: {
+						display: true,
+						text: 'rlimit-count'
+					},
                     scales: {
                         xAxes: [{
                             type: 'linear',
                             position: 'bottom'
                         }]
+                    },
+                    plugins: {
+                        zoom: {
+                            // Container for pan options
+                            pan: {
+                                // Boolean to enable panning
+                                enabled: true,
+
+                                // Panning directions. Remove the appropriate direction to disable
+                                // Eg. 'y' would only allow panning in the y direction
+                                mode: 'xy',
+                                rangeMin: {
+                                    // Format of min pan range depends on scale type
+                                    x: null,
+                                    y: null
+                                },
+                                rangeMax: {
+                                    // Format of max pan range depends on scale type
+                                    x: null,
+                                    y: null
+                                },
+                                // Function called once panning is completed
+                                // Useful for dynamic data loading
+                                onPan: function({chart}) { console.log(`I was panned!!!`); }
+                            },
+                            zoom: {
+                                enabled: true,
+                                drag: true,
+                                mode: 'xy',
+                                rangeMin: {
+                                    // Format of min zoom range depends on scale type
+                                    x: null,
+                                    y: null
+                                },
+                                rangeMax: {
+                                    // Format of max zoom range depends on scale type
+                                    x: null,
+                                    y: null
+                                },
+                                speed: 0.1
+                            }
+                        }
                     },
                     tooltips: {
                         callbacks: {
@@ -67,6 +146,8 @@ function downloadPerfData(buildId) {
                     }
                 }
             });
+
+            window.myLine = myChart;
         }
 
         // Make page visible
@@ -77,6 +158,10 @@ function downloadPerfData(buildId) {
         $('#loading').text("Error. No performance data available.");
     });
 }
+
+$("#resetZoom").click(function() {
+    window.myLine.resetZoom();
+});
 
 /// Runs once document is ready.
 $(document).ready(function() {
