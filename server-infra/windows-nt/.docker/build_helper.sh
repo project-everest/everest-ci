c58f724085b78057e7574617e086143aef95d0e7
Tahina Ramananandro <taramana@microsoft.com>
2019-10-14 11:19:47 -0700

    yet another error handling attempt

--- server-infra/windows-nt/.docker/build_helper.sh
+++ server-infra/windows-nt/.docker/build_helper.sh
@@ -16,9 +16,11 @@ echo $(date -u "+%Y-%m-%d %H:%M:%S") >> $out_file
 
 tail -f $out_file &
 tail_pd=$!
-{ { { { { { stdbuf -e0 -o0 ./build.sh ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDERR]!' ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDOUT]!' ; } 2>&1 ; } >> $out_file
+exitcode=0
+{ { { { { { stdbuf -e0 -o0 ./build.sh ; exitcode=$? ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDERR]!' ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDOUT]!' ; } 2>&1 ; } >> $out_file
 echo "Build finished" >> $out_file
 kill $tail_pd
+[[ $exitcode -eq 0 ]] # necessary because sed pipes above reset the error code
 
 echo "======= TRYING TO GET THE END OF THE LOG ======"
 tail -n 100 $out_file
