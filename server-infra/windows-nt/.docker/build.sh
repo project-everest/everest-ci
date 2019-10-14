21ab67ba2479553799fa03b58554aa09f6268896
Tahina Ramananandro <taramana@microsoft.com>
2019-10-14 11:21:03 -0700

    the first call to everest check is known to fail

--- server-infra/windows-nt/.docker/build.sh
+++ server-infra/windows-nt/.docker/build.sh
@@ -1,6 +1,6 @@
 set -e
 export OCAMLRUNPARAM=b
-./everest/everest --yes check 2>&1
+{ ./everest/everest --yes check 2>&1 ; } || true # this one is known to fail
 eval $(opam config env)
 ./everest/everest --yes check
 echo "End of build.sh"
