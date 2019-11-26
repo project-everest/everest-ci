5bd348ad50906d160b447eb37006d687039cc182
Tahina Ramananandro <taramana@microsoft.com>
2019-11-25 19:24:54 -0800

    nit

--- server-infra/windows-nt/.docker/build_helper.sh
+++ server-infra/windows-nt/.docker/build_helper.sh
@@ -19,7 +19,7 @@ echo $(date -u "+%Y-%m-%d %H:%M:%S")
 
 result_file="result.txt"
 success=false
-{ { { { { { stdbuf -e0 -o0 ./build.sh "$@" && success=true ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDERR]' ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDOUT]' ; } 2>&1 ; } | tee $out_file
+{ { { { { { stdbuf -e0 -o0 ./build.sh "$@" && success=true ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDERR]!' ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDOUT]!' ; } 2>&1 ; } | tee $out_file
 if $success ; then
     echo Success > $result_file
 else
