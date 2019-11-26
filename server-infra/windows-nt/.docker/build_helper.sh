b147d2f1714e54d329763c06edb8cb21e79a4b04
Tahina Ramananandro <taramana@microsoft.com>
2019-11-25 18:53:13 -0800

    restore build logs

--- server-infra/windows-nt/.docker/build_helper.sh
+++ server-infra/windows-nt/.docker/build_helper.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+target=$1
+out_file=$2
+threads=$3
+branchname=$4
 
 # Add ssh identity
 identity_added=false
@@ -13,7 +17,15 @@ fi
 
 echo $(date -u "+%Y-%m-%d %H:%M:%S")
 
-./build.sh
+result_file="result.txt"
+success=false
+{ { { { { { stdbuf -e0 -o0 ./build.sh "$@" && success=true ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDERR]' ; } 3>&1 1>&2 2>&3 ; } | sed -u 's!^![STDOUT]' ; } 2>&1 ; } | tee $out_file
+if $success ; then
+    echo Success > $result_file
+else
+    echo Failure > $result_file
+    exit 1
+fi
 
 echo "Build finished"
 
