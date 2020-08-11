9d70ab852fc05bdcd07890e0d55c90ff661b0af5
Tahina Ramananandro <taramana@microsoft.com>
2020-08-11 12:59:18 -0700

    attempt to install a minimal Windows CI machine

---
+++ server-infra/windows-nt-no-docker/bootstrap.ps1
@@ -0,0 +1,45 @@
+# Bootstrap a fresh Windows Server OS to become a build agent server.
+
+$Error.Clear()
+$LastExitCode = 0
+
+$ProgressPreference = 'SilentlyContinue'
+Write-Host "==== Bootstrap ===="
+
+# powershell defaults to TLS 1.0, which many sites don't support.  Switch to 1.2.
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# create /home/builder/build if needed
+$build_dir = "/home/builder/build"
+mkdir -Force $build_dir | Out-Null
+Set-Location $build_dir
+
+Write-Host "Install Azure CLI if not present."
+$azExists = (Get-Command az -ErrorAction:SilentlyContinue)
+if ($null -eq $azExists) {
+    $Error.Clear()
+
+    # install azure CLI
+    Write-Host "Installing Azure CLI"
+    wget "https://aka.ms/installazurecliwindows" -outfile "azurecli.msi"
+    Start-Process msiexec.exe -Wait -ArgumentList "/i azurecli.msi /passive"
+    Remove-Item "azurecli.msi"
+
+    if ($Error.Count -gt 0 -or $LastExitCode -ne 0) {
+        $Error
+        return
+    }
+}
+
+Write-Host "Install Cygwin with git"
+$Error.Clear()
+wget "https://www.cygwin.com/setup-x86_64.exe" -outfile "cygwinsetup.exe"
+Start-Process .\cygwinsetup.exe -Wait -ArgumentList "-q --root C:\cygwin64 -P git,wget -X"
+Remove-Item "cygwin64.exe"
+if ($Error.Count -gt 0 -or $LastExitCode -ne 0) {
+    $Error
+    return
+}
+
+Write-Host "Bootstrap done."
+$Error.Clear()
---
+++ server-infra/windows-nt-no-docker/vstsbuildagentsetup.ps1
@@ -0,0 +1,86 @@
+# This script is responsible to do the complete setup in order
+# to have build agents running on the Windows build machine.
+
+param
+(
+    [Parameter(Mandatory=$true, HelpMessage="VSTS Personal Access Token")]
+    [string] $vstsPat,
+    [Parameter(Mandatory=$false, HelpMessage="Agent pool name")]
+    [string] $poolName = "Msr-EverestPool-Windows-20200811",
+    [Parameter(Mandatory=$false, HelpMessage="Initial pool name index")]
+    [int] $initialPoolIndex = 1,
+    [Parameter(Mandatory=$false, HelpMessage="Finally pool name index")]
+    [int] $finalPoolIndex = 1
+)
+
+$Error.Clear()
+$LastExitCode = 0
+
+Write-Host PoolName = $poolName
+Write-Host InitialPoolIndex = $initialPoolIndex
+Write-Host FinalPoolIndex = $finalPoolIndex
+
+function ConfigAgents {
+    Param ([string] $vstsPat, [string] $poolName, [string] $agentName, [bool] $shouldRemove)
+
+    Write-Host "$agentName on $poolName"
+
+    $args = "--unattended --url https://msr-project-everest.visualstudio.com --auth path --token $vstsPat --pool $poolName --agent $agentName --acceptTeeEula --runAsService"
+    if ($shouldRemove) {
+        $args = "remove", $args
+    }
+
+    Start-Process "$((Get-Location).Path)\config.cmd" -NoNewWindow -Wait -ArgumentList $args
+}
+
+$originalLocation = "$((Get-Location).Path)"
+
+.\bootstrap.ps1
+
+if ($Error.Count -gt 0 -or $LastExitCode -ne 0) {
+    $Error
+    return
+}
+
+# create /home/builder/build/agents
+$agents_dir = "$($(Get-Location).Path)\agents"
+mkdir -Force $agents_dir | Out-Null
+
+# download the VSTS windows agent to that directory
+write-host "Downloading VSTS Windows Agent"
+wget "https://vstsagentpackage.azureedge.net/agent/2.148.1/vsts-agent-win-x64-2.148.1.zip" -outfile "$agents_dir\vsts-agent.zip"
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+# for each agent
+#  create agent-# subdir
+#  copy the agent binary into the subdir and extract from the downloaded .zip
+for ($i=$initialPoolIndex; $i -le $finalPoolIndex; $i++) {
+    $agent = "$((Get-Location).Path)\agents\agent-$i"
+    if ((Test-Path "$agent") -eq $false) {
+        mkdir "$agent" | Out-Null
+        write-host "Unzipping agent $i on $agent"
+        [System.IO.Compression.ZipFile]::ExtractToDirectory("$agents_dir\vsts-agent.zip", "$agent")
+    }
+}
+
+Remove-Item "$agents_dir\vsts-agent.zip"
+
+Write-Host "Setup all agents."
+$agentsFolder = "/home/builder/build/agents"
+for ($i=$initialPoolIndex; $i -le $finalPoolIndex; $i++) {
+
+    # First we add regular agent
+    $agentName="agent-$i"
+    Set-Location "$agentsFolder\$agentName"
+    ConfigAgents $vstsPat $poolName $agentName $true
+    ConfigAgents $vstsPat $poolName $agentName $false
+
+    if ($Error.Count -gt 0 -or $LastExitCode -ne 0) {
+        $Error
+        return
+    }
+}
+
+write-host "Done with setup."
+Set-Location $originalLocation
